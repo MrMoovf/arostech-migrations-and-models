68f248a2dda156036c1ad181155592b68a372bef
MrMoovf <anderskozuch1998@gmail.com>
2024-04-07 16:31:17 +0200

    yo

--- src/MigrationsServiceProvider.php
+++ src/MigrationsServiceProvider.php
@@ -11,9 +11,9 @@ class MigrationsServiceProvider extends ServiceProvider {
      */
     public function boot(): void
     {
-        $this->publishesMigrations([
-            __DIR__.'/../database/migrations/2024_01_27_122050_create_images_table.php' => database_path('migrations'),
-        ]);
+        $this->publishes([
+            __DIR__.'/database/migrations' => database_path('migrations'),
+        ],'migrations');
     }
 }
 
