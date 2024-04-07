823b0c7326db88edb832bc48e428d157bb11be1f
MrMoovf <anderskozuch1998@gmail.com>
2024-04-07 16:52:27 +0200

    dd

--- src/database/migrations/2023_11_09_223740_create_contents_table.php
+++ src/database/migrations/2023_11_09_223740_create_contents_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('content', function (Blueprint $table) {
+        Schema::create('contents', function (Blueprint $table) {
             $table->id();
             $table->string('img')->nullable(); //ny
             $table->string('title');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('content');
+        Schema::dropIfExists('contents');
     }
 };
