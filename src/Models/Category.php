23fae853e0edac19d5ca9260e3c4f0ef2d0e8ac2
MrMoovf <anderskozuch1998@gmail.com>
2024-05-30 17:33:08 +0200

    dd

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -5,10 +5,11 @@ namespace Arostech\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, HasRecursiveRelationships;
     protected $fillable = [
         'name',
         'parent_id'
