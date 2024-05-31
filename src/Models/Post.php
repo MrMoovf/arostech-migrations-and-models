2ac4d20a5ca7d657ef865930a0f530966c377258
MrMoovf <anderskozuch1998@gmail.com>
2024-05-31 16:17:27 +0200

    relation

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -5,6 +5,7 @@ namespace Arostech\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
@@ -28,5 +29,9 @@ class Post extends Model
 
     public function categories(): BelongsToMany {
         return $this->belongsToMany(Category::class);
+    }
+
+    public function featuredImage():HasOne{
+        return $this->hasOne(Image::class,'id');
     }
 }
