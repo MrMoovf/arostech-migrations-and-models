3f49ec4ec128822c36a62174771d7a7b594eb4ae
MrMoovf <anderskozuch1998@gmail.com>
2024-06-01 12:09:48 +0200

    Update 2024_05_13_211913_create_categories_table.php

--- src/database/migrations/2024_05_13_211913_create_categories_table.php
+++ src/database/migrations/2024_05_13_211913_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('hexcolor');
+            $table->string('hexcolor')->default('#000000');
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->timestamps();
 
